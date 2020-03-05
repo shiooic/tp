fa863dfc04f6ab2cdb5d918d4c3103f2a7637f50
shiooc <shiooc@gmail.com>
2020-03-05 15:25:10 +0800

    validateorder

--- application/admin/validate/Order.php
+++ application/admin/validate/Order.php
@@ -6,6 +6,12 @@ use think\Validate;
 
 class Order extends Validate
 {
-
+    protected $rule = [
+        ['product' , 'require', '产品名不能为空'],
+        ['num' , 'require|number','数量必须是数字'],
+        ['username' , 'require', '收件人不能为空'],
+        ['address' , 'require', '收货地址不能为空'],
+        ['tel','require|max:11|/^1[3-8]{1}[0-9]{9}$/','请输入手机号码|手机号码最多不能超过11个字符|手机号码格式不正确']
+    ];
 
 }
